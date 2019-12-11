1a5cdd2b50bfc97be6de84983094814738309b5d
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2019-12-11 09:09:05 -0600

    BOD - 12/11/19 - 00
    @tri-vi
    
    - Update summary-gene-panel block layout.
    - Update css for the entire layout.
    - Update index.js

--- index.js
+++ index.js
@@ -24,11 +24,15 @@ var reportHeader = ('./views/reports/report_header.ejs');
 var cvd_logo = "./public/img/cvdlogo-outlined.png";
 var bcm_logo = "./public/img/bcmlogo.jpg";
 var hgsc_logo = "./public/img/hgsclogo.png";
+var negativeGenePanelIcon = "./public/img/report_icons/negative_gene_panel_icon.png";
+var positiveGenePanelIcon = "./public/img/report_icons/positive_gene_panel_icon.png";
 
 // Base64 Images
 var base64_cvd_logo = base64Img.base64Sync(cvd_logo);
 var base64_bcm_logo = base64Img.base64Sync(bcm_logo);
 var base64_hgsc_logo = base64Img.base64Sync(hgsc_logo);
+var base64_negativeGenePanelIcon =  base64Img.base64Sync(negativeGenePanelIcon);
+var base64_positiveGenePanelIcon =  base64Img.base64Sync(positiveGenePanelIcon);
 
 
 
@@ -76,7 +80,9 @@ try {
       cvd_logo: base64_cvd_logo,
       bcm_logo: base64_bcm_logo,
       hgsc_logo: base64_hgsc_logo,
-      qr_code: base64_qr_code
+      qr_code: base64_qr_code,
+      negativeGenePanelIcon: base64_negativeGenePanelIcon,
+      positiveGenePanelIcon: base64_positiveGenePanelIcon
     }, function(err,str){
       if(err){
         console.log(err);
