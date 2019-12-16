7d5f2b47155f0e60033ff0f9f5d0d24e2f9ef0f6
Tri-Vi <36706183+Tri-Vi@users.noreply.github.com>
2019-12-16 13:56:16 -0600

    MOD - 12/16/19 - 00
    @tri-vi
    
    - Refactor generatePDF function
    - Update main template and block layout

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 const ejs = require('ejs');
@@ -50,161 +46,178 @@ var base64_positivePgxIcon = base64Img.base64Sync(positivePgxIcon);
 var base64_negativePgxIcon = base64Img.base64Sync(negativePgxIcon);
 var base64_blankSignature = base64Img.base64Sync(blankSignature);
 
-try {
-  (async() => {
-    
+var isPhiChecked = true;
 
-    // Get User Data From User Controller;
-    var returnedUser = UserController.getUserById(1);
+var returnedReports = ReportController.getAllReport();
 
-    // Get Report Data From Report Controller;
-    var returnedReports = ReportController.getAllReport();
-    var selectedReport = returnedReports[0];
 
-    selectedReport.order = {
-      indications: "A, B, C",
-      patient: {
-        patientFirstName: "First",
-        patientMiddleName: "Middle",
-        patientLastName: "Last",
-        patientDOB: new Date(),
-        mrn: 'MRN_1234',
-        sex: 'Male'
-      },
-      physician: {
-        name: 'ABC',
-        address1: 'address1',
-        address2: 'address2',
-        city: 'city',
-        state: 'state',
-        zipcode: 'zipcode',
-        phone:'111-222-3333'
-      },
-      specimen: {
-        external_id: 'external_id',
-        accession_id:'accession_id',
-        type: 'Blood',
-        specimenCollectionDate: new Date()
+
+generatePDF(returnedReports);
+
+async function generatePDF(reports){
+  try {
+
+    for(var i = 0; i < reports.length; i++){
+      console.log(i);
+      // Get User Data From User Controller;
+      var returnedUser = UserController.getUserById(1);
+      var selectedReport = reports[i];
+      
+      //var selectedReport = returnedReports[i];
+      console.log(selectedReport.reportIdentifier);
+
+      selectedReport.order = {
+        indications: "A, B, C",
+        patient: {
+          patientFirstName: "First",
+          patientMiddleName: "Middle",
+          patientLastName: "Last",
+          patientDOB: new Date(),
+          mrn: 'MRN_1234',
+          sex: 'Male'
+        },
+        physician: {
+          name: 'ABC',
+          address1: 'address1',
+          address2: 'address2',
+          city: 'city',
+          state: 'state',
+          zipcode: 'zipcode',
+          phone:'111-222-3333'
+        },
+        specimen: {
+          external_id: 'external_id',
+          accession_id:'accession_id',
+          type: 'Blood',
+          specimenCollectionDate: new Date()
+        }
+      };
+      selectedReport.state = "Approved";
+      selectedReport.stateChanged = new Date();
+
+      var reportableVariants =  _reportableVariant(selectedReport.variants);
+
+      // QR Code
+      // var qr_string = "selectedReport.reportIdentifier";
+      var qr_string = "1234";
+      var qr_code = qr.imageSync(qr_string, {type: 'png'});
+
+      // Image Data URI
+      var dataBuffer = new Buffer.from(qr_code);
+      var mediaType = 'PNG';
+      var base64_qr_code = imageDataURI.encode(dataBuffer, mediaType);
+
+      //Footer Template
+      var footerTemplate = "";
+      var footerData = {
+        name: "HGSC Clinical Laboratory",
+        address: "One Baylor Plaza",
+        city: "Houston",
+        state: "TX",
+        zipCode: 77030,
+        phone: "713.798.6539",
+        fax: "713.798.5741",
+        website: "www.hgsc.bcm.edu",
+        email: " cvdgenomics@hgsc.bcm.edu",
+        copyright: `Copyright © 2018 HGSC Clinical Laboratory. All rights reserved. ` + selectedReport.footer
+      };
+
+      //Header Template
+      var headerTemplate = "";
+      var headerData = {
+        cvd_logo: base64_cvd_logo,
+        bcm_logo: base64_bcm_logo,
+        hgsc_logo: base64_hgsc_logo,
+        qr_code: base64_qr_code,
+        patientName: selectedReport.order.patient.patientFirstName + ' ' + selectedReport.order.patient.patientMiddleName + ' ' + selectedReport.order.patient.patientLastName,
+        isPhiChecked: isPhiChecked == 'true' || isPhiChecked == true ? true : false
+      };
+
+      // EJS - Render Header
+      await ejs.renderFile(reportHeader, {headerData: headerData}, function(err, str){
+        if(err){
+          db.winston.error("ReportController.findAll", {'user' : user, 'params' : params, 'error': err});
+        } else {
+          headerTemplate = str;
+        }
+      })
+
+      console.log('Rendered Header');
+
+      // EJS - Render Footer
+      await ejs.renderFile(reportFooter, {footerData: footerData}, function(err, str){
+        if(err){
+          db.winston.error("ReportController.findAll", {'user' : user, 'params' : params, 'error': err});
+        } else {
+          footerTemplate = str;
+        }
+      });
+
+      console.log('Rendered Footer');
+
+      // EJS - Render Report
+      await ejs.renderFile(template, {
+        user: returnedUser,
+        report: selectedReport,
+        isPhiChecked: isPhiChecked,
+        cvd_logo: base64_cvd_logo,
+        bcm_logo: base64_bcm_logo,
+        hgsc_logo: base64_hgsc_logo,
+        qr_code: base64_qr_code,
+        negativeGenePanelIcon: base64_negativeGenePanelIcon,
+        positiveGenePanelIcon: base64_positiveGenePanelIcon,
+        highPrsIcon: base64_highPrsIcon,
+        moderatePrsIcon: base64_moderatePrsIcon,
+        lowPrsIcon: base64_lowPrsIcon,
+        posLpaIcon: base64_posLpaIcon,
+        negLpaIcon: base64_negLpaIcon,
+        positivePgxIcon: base64_positivePgxIcon,
+        negativePgxIcon: base64_negativePgxIcon,
+        blankSignature: base64_blankSignature,
+        reportableVariants: reportableVariants,
+        htmlString: '<h1 style="color:red;">htmlSTring<br> test 1<br>test 2<h1><h2>Header 2</h2><h3 style="color:blue;">Header 3</h3>'
+
+      }, function(err,str){
+        if(err){
+          console.log(err);
+        } else {
+          html = str;
+        }
+      });
+
+      console.log('Rendered Report');
+      
+      // Generating pdf using puppeteer
+      const browser = await puppeteer.launch({ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+
+      const page = await browser.newPage();
+      await page.setContent(html, {
+        waitUntil: 'networkidle0'
+      });
+
+      var path = "example_" + i + ".pdf";
+      await page.pdf({
+        path: path,
+        format: 'Letter',
+        scale: .75,
+        printBackground: true,
+        displayHeaderFooter: true,
+        headerTemplate: headerTemplate,
+        footerTemplate: footerTemplate,
+        margin: {
+          top: "130px",
+          bottom: "180px"
+        }
+      });
+
+      console.log('Generated PDF');
+      await page.close();
+      await browser.close();
+      console.log('Finished');
       }
-    };
-    selectedReport.state = "Approved";
-    selectedReport.stateChanged = new Date();
-
-    var reportableVariants =  _reportableVariant(selectedReport.variants);
-
-    // QR Code
-    // var qr_string = "selectedReport.reportIdentifier";
-    var qr_string = "1234";
-    var qr_code = qr.imageSync(qr_string, {type: 'png'});
-
-    // Image Data URI
-   var dataBuffer = new Buffer.from(qr_code);
-   var mediaType = 'PNG';
-   var base64_qr_code = imageDataURI.encode(dataBuffer, mediaType);
-
-   var isPhiChecked = true
-
-   //Footer Template
-   var footerTemplate = "";
-   var footerData = {
-     name: "HGSC Clinical Laboratory",
-     address: "One Baylor Plaza",
-     city: "Houston",
-     state: "TX",
-     zipCode: 77030,
-     phone: "713.798.6539",
-     fax: "713.798.5741",
-     website: "www.hgsc.bcm.edu",
-     email: " cvdgenomics@hgsc.bcm.edu",
-     copyright: `Copyright © 2018 HGSC Clinical Laboratory. All rights reserved. ` + selectedReport.footer
-   };
-
-   //Header Template
-   var headerTemplate = "";
-   var headerData = {
-     cvd_logo: base64_cvd_logo,
-     bcm_logo: base64_bcm_logo,
-     hgsc_logo: base64_hgsc_logo,
-     qr_code: base64_qr_code,
-     patientName: selectedReport.order.patient.patientFirstName + ' ' + selectedReport.order.patient.patientMiddleName + ' ' + selectedReport.order.patient.patientLastName,
-     isPhiChecked: isPhiChecked == 'true' || isPhiChecked == true ? true : false
-   };
-
-   // EJS - Render Header
-   await ejs.renderFile(reportHeader, {headerData: headerData}, function(err, str){
-    if(err){
-      db.winston.error("ReportController.findAll", {'user' : user, 'params' : params, 'error': err});
-    } else {
-      headerTemplate = str;
-    }
-  })
-
-  // EJS - Render Footer
-  await ejs.renderFile(reportFooter, {footerData: footerData}, function(err, str){
-    if(err){
-      db.winston.error("ReportController.findAll", {'user' : user, 'params' : params, 'error': err});
-    } else {
-      footerTemplate = str;
-    }
-  });
-    
-  // EJS - Render Report
-    await ejs.renderFile(template, {
-      user: returnedUser,
-      report: selectedReport,
-      isPhiChecked: isPhiChecked,
-      cvd_logo: base64_cvd_logo,
-      bcm_logo: base64_bcm_logo,
-      hgsc_logo: base64_hgsc_logo,
-      qr_code: base64_qr_code,
-      negativeGenePanelIcon: base64_negativeGenePanelIcon,
-      positiveGenePanelIcon: base64_positiveGenePanelIcon,
-      highPrsIcon: base64_highPrsIcon,
-      moderatePrsIcon: base64_moderatePrsIcon,
-      lowPrsIcon: base64_lowPrsIcon,
-      posLpaIcon: base64_posLpaIcon,
-      negLpaIcon: base64_negLpaIcon,
-      positivePgxIcon: base64_positivePgxIcon,
-      negativePgxIcon: base64_negativePgxIcon,
-      blankSignature: base64_blankSignature,
-      reportableVariants: reportableVariants,
-      htmlString: '<h1 style="color:red;">htmlSTring<br> test 1<br>test 2<h1><h2>Header 2</h2><h3 style="color:blue;">Header 3</h3>'
-
-    }, function(err,str){
-      if(err){
-        console.log(err);
-      } else {
-        html = str;
-      }
-    });
-    
-    // Generating pdf using puppeteer
-    const browser = await puppeteer.launch({ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.setContent(html, {
-      waitUntil: 'networkidle0'
-    });
-    await page.pdf({
-      path: 'example.pdf',
-      format: 'Letter',
-      scale: .75,
-      printBackground: true,
-      displayHeaderFooter: true,
-      headerTemplate: headerTemplate,
-      footerTemplate: footerTemplate,
-      margin: {
-        top: "130px",
-        bottom: "180px"
-      }
-    });
-
-    await browser.close();
-    console.log('Finished');
-
-
-  })();
-} catch(err) {
-  console.log(err);
+  } catch (err){
+    console.log(err);
+  }
 }
 
 function _reportableVariant(variants){
